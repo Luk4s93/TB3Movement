665cbe63060ccfbfbf9dce1f84f519b3fce3d68a
Lukas <lukas.stark@fh-stralsund.de>
2018-11-28 14:31:01 +0100

    First commit

---
+++ src/Main.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2011, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of the Willow Garage, Inc. nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import rospy
+from geometry_msgs.msg import Twist
+import sys, select, termios, tty
+from std_msgs.msg import String
+
+BURGER_MAX_LIN_VEL = 0.22
+BURGER_MAX_ANG_VEL = 2.84
+
+LIN_VEL_STEP_SIZE = 0.01
+ANG_VEL_STEP_SIZE = 0.1
+
+LAST_LIN_VEL = 0.0
+LAST_ANG_VEL = 0.0
+
+TRAFFIC_SIGN_DETECTED = False
+
+msg = """
+TurtleBot3 activated
+--------------------
+
+Max. linear velocity: 0.22
+Max. angular velocity: 2.84
+"""
+
+e = """Communications Failed"""
+
+def vels(target_linear_vel, target_angular_vel):
+    return "currently:\tlinear vel %s\t angular vel %s " % (target_linear_vel,target_angular_vel)
+
+def makeSimpleProfile(output, input, slop):
+    if input > output:
+        output = min( input, output + slop )
+    elif input < output:
+        output = max( input, output - slop )
+    else:
+        output = input
+
+    return output
+
+def constrain(input, low, high):
+    if input < low:
+      input = low
+    elif input > high:
+      input = high
+    else:
+      input = input
+
+    return input
+
+#
+def checkLinearLimitVelocity(vel):
+    vel = constrain(vel, -BURGER_MAX_LIN_VEL, BURGER_MAX_LIN_VEL)
+    return vel
+
+
+def checkAngularLimitVelocity(vel):
+    vel = constrain(vel, -BURGER_MAX_ANG_VEL, BURGER_MAX_ANG_VEL)
+    return vel
+
+# traffic sign detected, do:
+def sign_controls(ros_data):
+
+    sign_vel = 0.0
+    TRAFFIC_SIGN_DETECTED = True
+
+    if ros_data == 'entry forbidden':
+        sign_vel = checkLinearLimitVelocity(0.0)
+    elif ros_data == 'main road':
+        sign_vel = checkLinearLimitVelocity(0.22)
+    elif ros_data == 'turn right':
+        sign_vel = checkLinearLimitVelocity(0.0)
+    elif ros_data == 'turn left':
+        sign_vel = checkLinearLimitVelocity(0.0)
+    elif ros_data == 'pedestrians':
+        sign_vel = checkLinearLimitVelocity(0.11)
+    elif ros_data == 'warning':
+        sign_vel = checkLinearLimitVelocity(0.11)
+    elif ros_data == 'no parking':
+        sign_vel = checkLinearLimitVelocity(0.0)
+    elif ros_data == 'bus stop':
+        sign_vel = checkLinearLimitVelocity(0.0)
+    elif ros_data == 'crossing':
+        sign_vel = checkLinearLimitVelocity(0.11)
+    elif ros_data == 'slippery':
+        sign_vel = checkLinearLimitVelocity(0.11)
+    elif ros_data == 'road closed':
+        sign_vel = checkLinearLimitVelocity(0.0)
+    else:
+        TRAFFIC_SIGN_DETECTED = False
+
+    return sign_vel
+
+
+#
+def path_control():
+    path_ang = 0.0
+    return path_ang
+
+
+class burger_control:
+    def __init__(self):
+       self.raspi_subscriber = rospy.Subscriber("/traffic_sign/detected", String, self.callback)
+           rospy.loginfo("Subscribed to /traffic_sign/detected")
+
+    def callback(self, ros_data):
+        if ros_data != 'nothing':
+            target_linear_vel = sign_controls(ros_data)
+
+        rospy.loginfo(ros_data)
+
+if __name__=="__main__":
+    settings = termios.tcgetattr(sys.stdin)
+
+    rospy.init_node('turtlebot3_teleop')
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
+    turtlebot3_model = rospy.get_param("model", "burger")
+
+    status = 0
+    target_linear_vel   = 0.0
+    target_angular_vel  = 0.0
+    control_linear_vel  = 0.0
+    control_angular_vel = 0.0
+
+    control = burger_control()
+
+    '''try:
+        print msg
+        while(1):
+            key = getKey()
+            if key == 'w' :
+                target_linear_vel = checkLinearLimitVelocity(target_linear_vel + LIN_VEL_STEP_SIZE)
+                status = status + 1
+                print vels(target_linear_vel,target_angular_vel)
+            elif key == 'x' :
+                target_linear_vel = checkLinearLimitVelocity(target_linear_vel - LIN_VEL_STEP_SIZE)
+                status = status + 1
+                print vels(target_linear_vel,target_angular_vel)
+            elif key == 'a' :
+                target_angular_vel = checkAngularLimitVelocity(target_angular_vel + ANG_VEL_STEP_SIZE)
+                status = status + 1
+                print vels(target_linear_vel,target_angular_vel)
+            elif key == 'd' :
+                target_angular_vel = checkAngularLimitVelocity(target_angular_vel - ANG_VEL_STEP_SIZE)
+                status = status + 1
+                print vels(target_linear_vel,target_angular_vel)
+            elif key == ' ' or key == 's' :
+                target_linear_vel   = 0.0
+                control_linear_vel  = 0.0
+                target_angular_vel  = 0.0
+                control_angular_vel = 0.0
+                print vels(target_linear_vel, target_angular_vel)
+            else:
+                if (key == '\x03'):
+                    break
+
+            if status == 20 :
+                print msg
+                status = 0
+
+            twist = Twist()
+
+            control_linear_vel = makeSimpleProfile(control_linear_vel, target_linear_vel, (LIN_VEL_STEP_SIZE/2.0))
+            twist.linear.x = control_linear_vel; twist.linear.y = 0.0; twist.linear.z = 0.0
+
+            control_angular_vel = makeSimpleProfile(control_angular_vel, target_angular_vel, (ANG_VEL_STEP_SIZE/2.0))
+            twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = control_angular_vel
+
+            pub.publish(twist)
+
+    except:
+        print e
+
+    finally:
+        twist = Twist()
+        twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
+        twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
+        pub.publish(twist)
+    '''
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
