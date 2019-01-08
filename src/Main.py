3da489a16c1ddaaa2efd072af7e6a44b2fc9a792
8Brainless8 <steve.sydow@fh-stralsund.de>
2019-01-08 14:16:00 +0100

    trying to get values from lane_assist (line_follower)

--- src/Main.py
+++ src/Main.py
@@ -143,13 +143,13 @@ class burger_control:
 
     # lane detection, set angular velocity
     def lane_detection(self, ros_angular):
-        rospy.loginfo(self, ros_angular.data)
-        if ros_angular.data == 90:
+        rospy.loginfo(self, ros_angular)
+        if ros_angular == 90:
             self.last_angular = 0
         elif ros_angular < 90:
-            self.last_angular = ros_angular.data/180 * (-2.84)
+            self.last_angular = ros_angular/180 * (-2.84)
         elif ros_angular.data > 90:
-            self.last_angular = ros_angular.data/180 * 2.84
+            self.last_angular = ros_angular/180 * 2.84
 
         return self.last_angular
 
